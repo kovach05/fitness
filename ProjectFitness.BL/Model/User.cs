80aacc17f3aceef707b0345bbc5e907b227b7176
Max Kovach <makskovach049@gmail.com>
2023-12-22 22:57:32 +0100

    third commit

--- ProjectFitness.BL/Model/User.cs
+++ ProjectFitness.BL/Model/User.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectFitness.BL
+namespace ProjectFitness.BL.Model
 {
-    public class Class1
+    internal class User
     {
     }
 }
