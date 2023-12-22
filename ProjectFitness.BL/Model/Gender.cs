4f0195e7f387a629d6d0f25a01828f12b0e417eb
Max Kovach <makskovach049@gmail.com>
2023-12-22 23:07:40 +0100

    third commit2

--- ProjectFitness.BL/Model/Gender.cs
+++ ProjectFitness.BL/Model/Gender.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace ProjectFitness.BL.Model
 {
-    internal class Gender
+    /// <summary>
+    /// Стать
+    /// </summary>
+    public class Gender
     {
+        /// <summary>
+        /// Назва
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Створити нову стать
+        /// </summary>
+        /// <param name="name"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("The gender name cannot be empty or null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
--- ProjectFitness.BL/Model/User.cs
+++ ProjectFitness.BL/Model/User.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectFitness.BL.Model
 {
-    internal class User
+    public class User
     {
+        public string Name { get; }
+
+        public Gender Gender { get; }
+        public DateTime BirthDate { get; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
     }
 }
